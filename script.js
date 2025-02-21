878c8ecfabd8fa27493ca3fe4f9b1d38ada86173
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-21 10:13:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let searchDiv = document.querySelector(".search");
+let button = document.querySelector(".btn");
+let input = document.querySelector(".input");
+
+button.addEventListener("click", () => {
+    searchDiv.classList.add("active");
+    input.focus();
+});
